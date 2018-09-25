69ef2330cc9370e32fd2eb3a19163b7f911b3f7c
Ido Kleinman <idokleinman@gmail.com>
2018-09-24 17:10:50 -0700

    Base support for new Network object in SDK

---
+++ ParticleSDK/SDK/ParticleNetwork.h
@@ -0,0 +1,13 @@
+//
+//  ParticleNetwork.h
+//  ParticleSDKPods
+//
+//  Created by Ido Kleinman on 9/24/18.
+//  Copyright © 2018 Particle Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParticleNetwork : NSObject
+
+@end
---
+++ ParticleSDK/SDK/ParticleNetwork.m
@@ -0,0 +1,13 @@
+//
+//  ParticleNetwork.m
+//  ParticleSDKPods
+//
+//  Created by Ido Kleinman on 9/24/18.
+//  Copyright © 2018 Particle Inc. All rights reserved.
+//
+
+#import "ParticleNetwork.h"
+
+@implementation ParticleNetwork
+
+@end
